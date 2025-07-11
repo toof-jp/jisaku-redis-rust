94b460ddfc031287f304eb2357e4b082e5fc8f2b
toof <toof@toof.jp>
2025-07-11 18:51:44 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
